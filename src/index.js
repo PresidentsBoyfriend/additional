c3e7abac248c6cda61d2e1f10f102444eb84cac7
PresidentsBoyfriend <fs.as.8585@gmail.com>
2018-09-29 21:17:22 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,42 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
     var chars = str.split(''),
         stack = [],
-        open = ['{', '(', '[' , '|' ],
-        close = ['}', ')', ']' , '|'],
+        open = [],
+        close = [],
         closeIndex,
         openIndex;
-       var counter = 0,
-       counter2 = 0;
-       var chars2 = bracketsConfig.join();
-       var chars3 = chars2.split('');
-       var alphaNumeric = chars.concat(chars3);
-    for (var i = 0, len = alphaNumeric.length; i < len; i++) {
-       openIndex = open.indexOf(alphaNumeric[i]);
-        if (openIndex !== -1) {
-        	if ( openIndex == '|') {
-        		counter2++;
-        	}
-        	counter++;
-           stack.push(openIndex);
-           continue;
-       }
-
-       closeIndex = close.indexOf(alphaNumeric[i]);
-       if (closeIndex !== -1) {
-       	if (closeIndex == '|') {
-       		counter2--;
-       	}
-           openIndex = stack.pop();
-           if (closeIndex !== openIndex || counter2!==0) {
-               return false;
-           }
-       }
+        var counter = 0;
+        for ( var i = 0 ; i < bracketsConfig.length ; i++) {
+      open[i] = bracketsConfig[i][0];
+      close[i] = bracketsConfig[i][1];  
     }
-    
-    if (counter==0) {
-    	return false;
+    var counter = 0;
+    for ( var y = 1; y<chars.length; y++){
+    //    alert ( "HEREEEE");
+    //    alert (chars + " y = " + y);
+    //    counter++;
+      for ( var i = 0 ; i < open.length ; i++) {
+         // alert (chars[y-1] + "  " + open[i] + "  " + chars[y] + "  " +close [i]);
+        if (chars[y-1] == open[i] && chars[y] == close [i]) {
+          chars.splice(y-1,2); 
+          y=y-2;
+           // alert ("MB ELSE WORK?" + "  counter = "+counter + "  y = "+y);
+           // counter = 0;
+        }
+      }
     }
-    if (stack.length !== 0) {
+    if (chars.length !== 0) {
         return false;
     }
 
