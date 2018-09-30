bd0c226f7ca0b4d01f74b6b5f2ccaa5cf07dacca
PresidentsBoyfriend <fs.as.8585@gmail.com>
2018-09-30 10:55:57 +0300

    third commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
     var chars = str.split(''),
-        stack = [],
         open = [],
-        close = [],
-        closeIndex,
-        openIndex;
+        close = [];
         var counter = 0;
         for ( var i = 0 ; i < bracketsConfig.length ; i++) {
       open[i] = bracketsConfig[i][0];
@@ -12,22 +9,15 @@ module.exports = function check(str, bracketsConfig) {
     }
     var counter = 0;
     for ( var y = 1; y<chars.length; y++){
-    //    alert ( "HEREEEE");
-    //    alert (chars + " y = " + y);
-    //    counter++;
       for ( var i = 0 ; i < open.length ; i++) {
-         // alert (chars[y-1] + "  " + open[i] + "  " + chars[y] + "  " +close [i]);
         if (chars[y-1] == open[i] && chars[y] == close [i]) {
           chars.splice(y-1,2); 
           y=y-2;
-           // alert ("MB ELSE WORK?" + "  counter = "+counter + "  y = "+y);
-           // counter = 0;
         }
       }
     }
     if (chars.length !== 0) {
         return false;
     }
-
     return true;
 }
